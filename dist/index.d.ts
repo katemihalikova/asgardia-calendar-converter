3f6e611eee6f42f312e1ad018523268d49dc0aaa
Kate Miháliková <kate@katemihalikova.cz>
2019-04-24 14:44:14 +0200

    chore: remove dist folder from repo

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Datum {
-    day: number;
-    month: number;
-    year: number;
-}
-export declare function isGregorianYearLeap(year: number): boolean;
-export declare function isAsgardianYearLeap(year: number): boolean;
-export declare function toAsgardian({day, month, year}: Datum, mode: "non" | "feb" | "jun"): Datum;
-export declare function toGregorian({day, month, year}: Datum, mode: "non" | "feb" | "jun"): Datum;
